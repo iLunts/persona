37c598c998644b32d83aa2d78756a8eb7c696726
Roman Luntsevich <shipik2008@gmail.com>
2021-11-28 02:44:33 +0300

    Created persona is your new beauty camera

---
+++ src/app/shared/components/blocks/beauty-camera/beauty-camera.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-block-beauty-camera',
+  templateUrl: './beauty-camera.component.html',
+  styleUrls: ['./beauty-camera.component.less'],
+})
+export class BeautyCameraComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/shared/components/blocks/blocks.module.ts
+++ src/app/shared/components/blocks/blocks.module.ts
@@ -1,27 +1,31 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './main/main.component';
+import { NgModule } from '@angular/core';
+import { NgwWowModule } from 'ngx-wow';
+import { NgxParallaxScrollModule } from 'ngx-parallax-scroll';
+
+import { BeautyCameraComponent } from './beauty-camera/beauty-camera.component';
 import { CameraComponent } from './camera/camera.component';
 import { FaceFilterComponent } from './face-filter/face-filter.component';
 import { FunMasksComponent } from './fun-masks/fun-masks.component';
+import { MainComponent } from './main/main.component';
 import { ModelsFavoriteAppComponent } from './models-favorite-app/models-favorite-app.component';
-import { NgxParallaxScrollModule } from 'ngx-parallax-scroll';
-import { NgwWowModule } from 'ngx-wow';
 
 @NgModule({
   declarations: [
-    MainComponent,
+    BeautyCameraComponent,
     CameraComponent,
     FaceFilterComponent,
     FunMasksComponent,
+    MainComponent,
     ModelsFavoriteAppComponent,
   ],
   imports: [CommonModule, NgxParallaxScrollModule, NgwWowModule],
   exports: [
-    MainComponent,
+    BeautyCameraComponent,
     CameraComponent,
     FaceFilterComponent,
     FunMasksComponent,
+    MainComponent,
     ModelsFavoriteAppComponent,
   ],
 })
